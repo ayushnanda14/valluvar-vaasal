0d723851755e9599af70cf9ff18607bf2bc2675c
Ayush Nanda <ayush@blod.in>
2025-03-25 14:52:17 +0530

    init commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <nav className="flex">
+      <div>
+        <span>Valluvar Vaasal</span>
+      </div>
+      <div>
+        hellow
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
